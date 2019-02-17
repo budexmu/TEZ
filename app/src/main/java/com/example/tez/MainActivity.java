da684e7be0465b3fc8d35b72f646e33c68d1021f
Your Name <demetnoyan55@gmail.com>
2019-02-17 12:12:06 +0300

    first

---
+++ app/src/main/java/com/example/tez/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.tez;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,0);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bm = (Bitmap) data.getExtras().get("data");
+        Intent intent=new Intent(this,ValidationScreen_Activity.class);
+        intent.putExtra("image",bm);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/example/tez/ValidationScreen_Activity.java
@@ -0,0 +1,38 @@
+package com.example.tez;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class ValidationScreen_Activity extends AppCompatActivity {
+       ImageView imageview;
+       Button b;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.validation_screen);
+
+        imageview = (ImageView) findViewById(R.id.imageView2);
+        Bitmap bmp = (Bitmap) getIntent().getParcelableExtra("image");
+        imageview.setImageBitmap(bmp);
+        b=(Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ValidationScreen_Activity.this,ProductLİstActivity.class);
+                startActivity(intent);
+            }
+        });
+
+          }
+}
